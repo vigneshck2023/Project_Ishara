b43a8846ef21aa1d1e5da4fc1240bff55799bce8
Vignesh C K <vigneshck2003.code@gmail.com>
2025-09-03 17:44:09 +0530

    changes

--- index.js
+++ index.js
@@ -104,7 +104,4 @@ app.delete("/api/wishlist/:id", async (req, res) => {
 });
 
 // Start Server
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+module.exports = app;
